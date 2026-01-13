25853b7775be8c10363b7bb69fbee8ddfb8100b6
Luca Sforza <lucasforza1234@icloud.com>
2026-01-13 21:32:42 +0100

    Rust and other extensions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,4 +9,37 @@ return {
       },
     },
   },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ft = "rust",
+    config = function()
+      local extension_path = "$HOME/.local/share/nvim/mason/bin/codelldb" .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      -- If you are on Linux, replace the line above with the line below:
+      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      local cfg = require("rustaceanvim.config")
+
+      vim.g.rustaceanvim = {
+        server = {
+          cmd = function()
+            return { "/usr/bin/rust-analyzer" }
+          end,
+        },
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end,
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
 }
